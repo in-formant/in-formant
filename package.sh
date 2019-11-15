172f528a8513e7b104e452a17ef06841d850c8dd
Clo Yun-Hee Dufour <chloedv99@gmail.com>
2019-11-15 12:18:46 +0100

    Added script to build and package binaries for a (pre)release.

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+./build-lin.sh
+./build-win-mxe.sh
+
+rm -rf pkg
+mkdir pkg
+cd pkg
+
+rm -rf speech-analysis
+mkdir -p speech-analysis
+cp ../cmake-build-release/speech_analysis speech-analysis/
+cp -r ../fonts speech-analysis/
+zip speech-analysis-lin.zip -r speech-analysis
+
+rm -rf speech-analysis
+mkdir -p speech-analysis
+cp ../cmake-build-release-win/speech_analysis.exe speech-analysis/
+cp ../cmake-build-release-win/*.dll speech-analysis/
+cp -r ../fonts speech-analysis/
+zip speech-analysis-win.zip -r speech-analysis
