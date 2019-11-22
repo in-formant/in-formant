9259e0a6a76e8adffba61bcc0fc2e4a08b95a5ad
Chloé Ichinose <chloedv99@gmail.com>
2019-11-22 14:54:55 +0100

    Longer analysis window.

--- src/audio/AudioCapture.h
+++ src/audio/AudioCapture.h
@@ -12,7 +12,7 @@
 #include <vector>
 #include "RingBuffer.h"
 
-#define CAPTURE_DURATION 35.0
+#define CAPTURE_DURATION 60.0
 #define CAPTURE_SAMPLE_COUNT(sampleRate) ((CAPTURE_DURATION * sampleRate) / 1000)
 
 #define BUFFER_SAMPLE_COUNT(sampleRate) (CAPTURE_SAMPLE_COUNT(sampleRate))
