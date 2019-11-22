1f28ac942c17a9d1bbe7a259f99d23463008c1ca
Chloé Ichinose <chloedv99@gmail.com>
2019-11-22 03:12:44 +0100

    Full name for package zip file names

--- package.sh
+++ package.sh
@@ -11,11 +11,13 @@ rm -rf speech-analysis
 mkdir -p speech-analysis
 cp ../cmake-build-release/speech_analysis speech-analysis/
 cp -r ../fonts speech-analysis/
-zip speech-analysis-lin.zip -r speech-analysis
+zip speech-analysis-linux.zip -r speech-analysis
 
 rm -rf speech-analysis
 mkdir -p speech-analysis
 cp ../cmake-build-release-win/speech_analysis.exe speech-analysis/
 cp ../cmake-build-release-win/*.dll speech-analysis/
 cp -r ../fonts speech-analysis/
-zip speech-analysis-win.zip -r speech-analysis
+zip speech-analysis-windows.zip -r speech-analysis
+
+rm -rf speech-analysis
