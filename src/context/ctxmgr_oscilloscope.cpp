560cac923a1161612f8c006bddd996a15c748b62
Clo Yun-Hee Dufour <contact@cloyunhee.fr>
2020-10-09 23:00:02 +0200

    Added period tracking to the oscilloscope

--- src/context/ctxmgr_oscilloscope.cpp
+++ src/context/ctxmgr_oscilloscope.cpp
@@ -11,19 +11,48 @@ void ContextManager::renderOscilloscope(RenderingContext &rctx)
 
     auto& sound = soundTrack[iframe];
     auto& glot = glotTrack[iframe];
-    int length;
 
-    length = sound.size();
+    // Find the last zero crossing in glot.
+    int glotZcr = glot.size() - 1;
 
-    Renderer::GraphRenderData graphRender(length);
-    for (int i = 0; i < length; ++i) {
+    for (int i = glot.size() - 1; i >= 1; --i) {
+        if (glot[i - 1] * glot[i] < 0 && glot[i - 1] > 0) {
+            glotZcr = i;
+            break;
+        }
+    }
+
+    // Find the second to last zero crossing in glot.
+    int glotZcr2 = glotZcr - 3;
+    for (int i = glotZcr2; i >= 1; --i) {
+        if (glot[i - 1] * glot[i] < 0 && glot[i - 1] > 0) {
+            glotZcr2 = i;
+            break;
+        }
+    }
+
+    int glotPeriod = std::max(glotZcr - glotZcr2, 10);
+    
+    static float prevGlotLength = glot.size();
+
+    int glotLength = (glotZcr / glotPeriod) * glotPeriod;
+
+    prevGlotLength = 0.08f * glotLength + 0.92f * prevGlotLength;
+    glotLength = std::floor(prevGlotLength);
+
+    int soundZcr = (glotZcr * sound.size()) / glot.size();
+    int soundZcr2 = (glotZcr2 * sound.size()) / glot.size();
+    int soundLength = (glotLength * sound.size()) / glot.size();
+
+    Renderer::GraphRenderData graphRender(soundLength);
+    for (int i = 0; i < soundLength; ++i) {
         graphRender[i] = {
             .x = static_cast<float>(i),
-            .y = sound[i] * 20.0f + 5.0f,
+            .y = sound[sound.size() - soundLength + i] * 20.0f + 5.0f,
         };
     }
     if (!graphRender.empty()) {
-        rctx.renderer->renderGraph(graphRender, 0, length - 1, 3.0f, 1.0f, 1.0f, 1.0f);
+        rctx.renderer->renderGraph(graphRender, 0, soundLength - 1, 3.0f, 1.0f, 1.0f, 1.0f);
     }
     else {
         graphRender.push_back({.x = 0.0f, .y = 5.0f});
@@ -31,17 +60,16 @@ void ContextManager::renderOscilloscope(RenderingContext &rctx)
         rctx.renderer->renderGraph(graphRender, 0, 1, 3.0f, 1.0f, 1.0f, 1.0f);
     }
 
-    length = glot.size(); 
-    graphRender.resize(length);
+    graphRender.resize(glotLength);
 
-    for (int i = 0; i < length; ++i) {
+    for (int i = 0; i < glotLength; ++i) {
         graphRender[i] = {
             .x = static_cast<float>(i),
-            .y = glot[i] * 2.5f - 5.0f,
+            .y = glot[glot.size() - glotLength + i] * 2.5f - 5.0f,
         };
     }
     if (!graphRender.empty()) {
-        rctx.renderer->renderGraph(graphRender, 0, length - 1, 3.0f, 1.0f, 0.5f, 0.0f);
+        rctx.renderer->renderGraph(graphRender, 0, glotLength - 1, 3.0f, 1.0f, 0.5f, 0.0f);
     } 
     else {
         graphRender.push_back({.x = 0.0f, .y = -5.0f});
