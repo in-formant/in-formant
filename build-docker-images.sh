844abff8be6665fedb836d686d9d0949961020c6
Clo Yun-Hee Dufour <cloyunhee@gmail.com>
2021-02-19 08:06:33 +0100

    Removed unnecessary extra build scripts

--- build-docker-images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd docker
-
-for tag in linux win32 win64 macos android; do
-    docker build -t clorika/sabuilder:$tag . -f Dockerfile.$tag
-    docker tag clorika/sabuilder:$tag docker.io/clorika/sabuilder:$tag
-    docker push clorika/sabuilder:$tag
-done
